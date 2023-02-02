03266a2f6c06338923d102748d463c2c90ba8d17
bmankowski <borys.mankowski@gmail.com>
2023-02-02 18:47:31 +0100

    created a package for the animals project - implemented a remove method in myLinkedList

--- src/abstract_classes/challenge/Main.java
+++ src/abstract_classes/challenge/Main.java
@@ -3,5 +3,16 @@ package abstract_classes.challenge;
 public class Main {
     public static void main(String[] args) {
 
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+        String stringData = "1 3 55 2 1 5 7 3";
+
+        String[] data = stringData.split(" ");
+        for(String s : data){
+            list.addItem(new Node(s));
+        }
+        list.traverse(list.getRoot());
+
     }
 }
